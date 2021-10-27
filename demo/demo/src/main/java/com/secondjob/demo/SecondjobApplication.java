a4f70ecbe65cc2d770230e47f254a03913f73351
DESKTOP-6BML9KC\Ravishankar <shankarvani98@gmail.com>
2021-10-27 11:10:36 -0400

    pomcommit

---
+++ demo/demo/src/main/java/com/secondjob/demo/SecondjobApplication.java
@@ -0,0 +1,13 @@
+package com.secondjob.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondjobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondjobApplication.class, args);
+	}
+
+}
---
+++ demo/demo/src/test/java/com/secondjob/demo/SecondjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secondjob.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
